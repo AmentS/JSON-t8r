08ae8208ffaf0180c8e6cdcabedd21d4dd80f877
Spasoje <spasoje.djurovic90@gmail.com>
2020-05-21 00:37:15 +0200

    Add  'vue-google-charts'

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -1,6 +1,9 @@
 import Vue from 'vue';
 import axios from 'axios';
 import VueRouter from 'vue-router';
+import VueGoogleCharts from 'vue-google-charts'
+
+Vue.use(VueGoogleCharts)
 
 
 window.Vue = Vue;
--- resources/js/components/AdminPage.vue
+++ resources/js/components/AdminPage.vue
@@ -1,14 +1,39 @@
 <template>
 
-    
+    <GChart
+        type="PieChart"
+        :data="chartData"
+        :options="chartOptions"
+    />
 
 
 </template>
 
 <script>
     export default {
-        name: "AdminPage"
+        data () {
+            return {
+                // Array will be automatically processed with visualization.arrayToDataTable function
+                chartData: [
+                    ['Task', 'Hours per Day'],
+                    ['Work',     11],
+                    ['Eat',      2],
+                    ['Commute',  2],
+                    ['Watch TV', 2],
+                    ['Sleep',    7]
+                ],
+                chartOptions: {
+                    chart: {
+                        title: 'Company Performance',
+                        subtitle: 'Sales, Expenses, and Profit: 2014-2017',
+                    }
+                }
+            }
+        }
     }
+
+
+
 </script>
 
 <style scoped>
