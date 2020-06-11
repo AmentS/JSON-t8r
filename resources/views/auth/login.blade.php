d4f9b6829c9e41d89f3aca0a095b6bb0bdedfe9f
Spasoje <spasoje.djurovic90@gmail.com>
2020-06-11 03:37:31 +0200

    Add picture login

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -4,12 +4,17 @@
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
+
             <div class="card">
-                <div class="card-header">{{ __('Login') }}</div>
+
+                <div class="card-header" style="background-image: url('images/login_test2.jpg'); width: 100%; height: 200px;"></div>
+
 
                 <div class="card-body">
+
                     <form method="POST" action="{{ route('login') }}">
                         @csrf
+
 
                         <div class="form-group row">
                             <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
@@ -52,6 +57,7 @@
                         </div>
 
                         <div class="form-group row mb-0">
+
                             <div class="col-md-8 offset-md-4">
                                 <button type="submit" class="btn btn-primary">
                                     {{ __('Login') }}
