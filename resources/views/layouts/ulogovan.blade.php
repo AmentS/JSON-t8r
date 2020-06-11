9985285066f1cfc875cb0f9644112144bcdb8c40
Spasoje <spasoje.djurovic90@gmail.com>
2020-06-11 03:37:54 +0200

    Add picture register

--- resources/views/layouts/ulogovan.blade.php
+++ resources/views/layouts/ulogovan.blade.php
@@ -3,12 +3,12 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-   
+
         <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/bulma/0.7.5/css/bulma.css">
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.12.1/css/all.min.css">
 
         <title>JSON-t8R</title>
-        
+
     </head>
     <body>
         <div id="app">
@@ -17,13 +17,13 @@
             <section class="section">
                  <div class="container">
                      <router-view>
-                
+
                     </router-view>
                  </div>
             </section>
 
-            
-            
+
+
         </div>
 
 
